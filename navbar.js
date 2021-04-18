ba4220f09c78bbf32a3996cc8a17ee702f689cb9
Lazysniper <igor.filipe.lima@gmail.com>
2021-04-18 15:03:45 +0100

    Update

--- navbar.js
+++ navbar.js
@@ -4,7 +4,7 @@ const myNav= document.querySelector('nav');
 addEventListener('scroll', () =>{
  
     if(scrollY>530){
-        myNav.style.setProperty('background',"#1e0d60");
+        myNav.style.setProperty('background',"#117711");
     }
   else {
         myNav.style.setProperty('background',"transparent");
